a06821169b6344b61793f8eac42ac72f6cd5a245
Frontend Engineer <158271304+ntswakikekana@users.noreply.github.com>
2025-04-24 13:14:23 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const greeting = document.getElementById("greeting");
+const hour = new Date().getHours();
+
+if (hour < 12) {
+  greeting.textContent = "Good Morning! 👋";
+} else if (hour < 18) {
+  greeting.textContent = "Good Afternoon! 👋";
+} else {
+  greeting.textContent = "Good Evening! 👋";
+}
